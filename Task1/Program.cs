401ab7aa31a880a089bdcd8b276396358a9d478b
Bronivick <mister.galiakhmetov2016@yandex.ru>
2023-10-02 23:51:21 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int Num = int.Parse(Console.ReadLine());
+
+int EndNum = 1;
+
+Console.WriteLine(PrintNumber(Num,EndNum));
+
+string PrintNumber(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return (start + "," + PrintNumber(start-1, end));
+}
