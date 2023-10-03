1309a206e364dcb87f82012a2ba0146b750bd112
Bronivick <mister.galiakhmetov2016@yandex.ru>
2023-10-03 04:23:49 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите значение M: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение N: ");
+int N = int.Parse(Console.ReadLine());
+
+SumFromMToN(M, N);
+
+void SumFromMToN(int M, int N)
+{
+ Console.Write(SumMN(M - 1, N));
+}
+
+int SumMN(int M, int N)
+{
+    int result = M;
+    if (M == N)
+        return 0;
+    else
+    {
+        M++;
+        result = M + SumMN(M, N);
+        return result;
+    }
+}
